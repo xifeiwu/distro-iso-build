34c6e8a7db3b054f4a858a248a49ad7925a8af4f
jianmin <jianmin@nfs.iscas.ac.cn>
2013-09-03 16:55:47 +0800

    The sh file for unzip iso file and make iso. It is successed to execute in my machine.

---
+++ jianmin/mkiso.sh
@@ -0,0 +1,35 @@
+#!/bin/echo warning:you should run as root. But be careful!
+
+cd $(dirname "$0")
+
+if [ ! -e $PWD/mymint ] ; then
+    echo error: mymint does not exist. exit.
+    exit
+fi
+
+if [ ! -e $PWD/mymint/casper ] ; then
+    echo error: mymint/casper does not exist. exit.
+    exit
+fi
+
+if [ ! -e $PWD/squashfs-root ] ; then
+    echo error: squashfs-root does not exist. exit.
+    exit
+fi
+
+echo generate manifest.
+chroot squashfs-root dpkg-query -W --showformat='${Package} ${Version}\n' > mymint/casper/filesystem.manifest
+cp mymint/casper/filesystem.manifest mymint/casper/filesystem.manifest-desktop
+
+echo mksquashfs
+rm -rf mymint/casper/filesystem.squashfs
+mksquashfs squashfs-root mymint/casper/filesystem.squashfs 
+
+echo gen md5sum
+cd mymint
+find . -type f -print0 | xargs -0 md5sum > md5sum.txt
+cd ..
+
+echo  mkisofs
+cd mymint
+mkisofs -r -V "mymint" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o "../mymint-1.0-i386.iso" .
---
+++ jianmin/uniso.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+ISOPATH=/home/j/Backup/linuxmint-15-cinnamon-dvd-32bit.iso
+echo warning:you should run as root. But be careful!
+
+
+cd $(dirname "$0")
+
+if [ ! -d mymint ] ; then
+    if [ ! -e mintiso ] ; then
+        echo mount iso to $PWD/mintiso
+        mkdir mintiso
+        mount -o loop $ISOPATH mintiso
+    else
+        echo warning:mintiso has exist, it is expected iso has been mounted normally.
+    fi
+
+    echo copy iso/* to mymint, just wait for some minutes.
+    mkdir mymint
+    cp -r mintiso/. mymint
+    umount mintiso
+    rmdir mintiso
+else
+    echo warning:mymint has exist, it is expected iso/* has been copied to mymint dir.
+fi
+
+if [ ! -e squashfs-root ] ; then
+    echo unsquashfs mymint/casper/filesystem.squashfs
+    echo just wait for some minutes.
+    unsquashfs mymint/casper/filesystem.squashfs
+else
+    echo warning:squashfs-root has exist, it is expected filesystem.squashfs has been executed unsquashfs normally.    
+fi
+

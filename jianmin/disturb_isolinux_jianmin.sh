85f9aed0d49b1b01db943c25128660eb3980c2c9
jianmin <jianmin@nfs.iscas.ac.cn>
2013-09-06 15:15:34 +0800

    Change to patch way to disturb isolinux.cfg

--- jianmin/disturb_isolinux_jianmin.sh
+++ jianmin/disturb_isolinux_jianmin.sh
@@ -2,10 +2,11 @@
 #change back_ground /usr/share/backgrounds/linuxmint-olivia
 
 OUTPATH=/home/j/pcos/mkiso_out
-DISTURBPATH=`dirname $0`
+DISTURBPATH=$(cd "$(dirname $0)"; pwd)
 
 echo change mymint/isolinux/splash.png
 cp $DISTURBPATH/isolinux/splash.jpg $OUTPATH/mymint/isolinux/splash.jpg
 
 echo change mymint/isolinux/isolinux.cfg
-cp $DISTURBPATH/isolinux/isolinux.cfg $OUTPATH/mymint/isolinux/isolinux.cfg
+cd $OUTPATH
+patch -p0 < $DISTURBPATH/isolinux/isolinux.cfg.patch

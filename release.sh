a874be44424ff5d8c6cc63d1b5e7a105d71fa3ee
jianmin <jianmin@nfs.iscas.ac.cn>
2013-09-12 15:18:40 +0800

    Add install vim in release.sh

--- release.sh
+++ release.sh
@@ -12,6 +12,7 @@ sudo sh $WORKPATH/uniso.sh $ISOPATH
 sudo sh $WORKPATH/release/installzh_CN.sh $OUTPATH $APPPATH
 sudo sh $WORKPATH/release/installwps.sh $OUTPATH $APPPATH
 sudo sh $WORKPATH/release/installchrome.sh $OUTPATH $APPPATH
+sudo sh $WORKPATH/release/installvim.sh $OUTPATH $APPPATH
 sudo sh $WORKPATH/release/change_welcome_slide.sh $OUTPATH
 sudo sh $WORKPATH/release/change_help_slide.sh $OUTPATH
 sudo sh $WORKPATH/release/change_isolinux_splash.sh $OUTPATH

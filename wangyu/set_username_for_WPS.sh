107bf594e8ea5265a6712f9d5e8c90975fc1ea13
Wang Yu <wangyu@nfs.iscas.ac.cn>
2013-09-18 11:14:14 +0800

    file for set default username for wps

---
+++ wangyu/set_username_for_WPS.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [ -z "$1" ] ; then
+    echo error: No outpath setting at first param.
+    exit -1
+fi
+
+OUTPATH=$1
+DISTURBPATH=$(cd "$(dirname $0)"; pwd)
+
+echo set default username for WPS, add $OUTPATH/squashfs-root/etc/skel/.config/Software/Kingsoft.conf
+mypath=$OUTPATH/squashfs-root/etc/skel/.config/Software
+if [ ! -x "$mypath" ] ; then
+    mkdir $mypath
+fi
+cp $DISTURBPATH/Kingsoft.conf $OUTPATH/squashfs-root/etc/skel/.config/Software/

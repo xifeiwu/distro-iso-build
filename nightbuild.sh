c426d02dc89fa3f384024865129ad445985f50bd
night <night@example.com>
2013-09-27 15:27:02 +0800

    new source iso

--- nightbuild.sh
+++ nightbuild.sh
@@ -2,7 +2,7 @@
 APPPATH=/home/box/Workspace/Public/app
 WORKPATH=$(cd "$(dirname $0)"; pwd)
 OUTPATH=/home/night/out
-ISOPATH=/home/box/Workspace/Public/linuxmint-15-cinnamon-dvd-32bit-1-4kernel.iso
+ISOPATH=/home/box/Workspace/Public/linuxmint-15-cinnamon-dvd-32bit-1-4kernel-1.iso
 LOGNAME="log`date +%Y%m%d%H%M`.txt"
 ISOOUTPATH=/home/night/isout
 if [ -d $OUTPATH/squashfs-root ] ; then

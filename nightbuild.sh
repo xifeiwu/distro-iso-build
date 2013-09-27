514bd9204c698d23be37bd3a58ac5a5afa3d7a0c
night <night@example.com>
2013-09-27 09:32:47 +0800

    change kernel nightbuild.sh

--- nightbuild.sh
+++ nightbuild.sh
@@ -3,7 +3,7 @@ set -e
 APPPATH=/home/night/app
 WORKPATH=$(cd "$(dirname $0)"; pwd)
 OUTPATH=/home/night/out
-ISOPATH=/home/box/Workspace/Public/linuxmint-15-cinnamon-dvd-32bit.iso
+ISOPATH=/home/box/Workspace/Public/linuxmint-15-cinnamon-dvd-32bit-1-4kernel.iso
 LOGNAME="log`date +%Y%m%d`.txt"
 ISOOUTPATH=/home/night/isout
 if [ -d $OUTPATH/squashfs-root ] ; then
@@ -17,5 +17,6 @@ fi
 if [ -d $OUTPATH/mymint ] ; then
     rm -f -r $OUTPATH/mymint
 fi
+cd $WORKPATH
 git pull
 sudo sh $WORKPATH/release.sh $ISOPATH $APPPATH $OUTPATH $ISOOUTPATH > ~/log/$LOGNAME 2>&1

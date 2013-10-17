2244e25a80bd8af6bedb72cc849aae702081bf0e
night <night@example.com>
2013-10-17 09:19:55 +0800

    change the mail name

--- nightbuild.sh
+++ nightbuild.sh
@@ -4,7 +4,7 @@ WORKPATH=$(cd "$(dirname $0)"; pwd)
 OUTPATH=/home/night/out
 ISOPATH=/home/box/Workspace/Public/linuxmint-15-cinnamon-dvd-32bit-1-4kernel-1.iso
 LOGNAME="log`date +%Y%m%d%H%M`.txt"
-MAILNAME="buildlog`date +%Y%m%d%H%M`"
+MAILNAME="《IBP-COS-buildlog》`date +%Y%m%d%H%M`"
 ISOOUTPATH=/home/night/isout
 if [ -d $OUTPATH/squashfs-root ] ; then
     rm -f -r $OUTPATH/squashfs-root
@@ -20,4 +20,4 @@ fi
 cd $WORKPATH
 git pull
 sudo sh $WORKPATH/release.sh $ISOPATH $APPPATH $OUTPATH $ISOOUTPATH > ~/log/$LOGNAME 2>&1
-cat ~/log/$LOGNAME | mutt -s $MAILNAME wangbo@iscas.ac.cn jianmin@nfs.iscas.ac.cn xifei@nfs.iscas.ac.cn wangfeng@nfs.iscas.ac.cn wangyu@nfs.iscas.ac.cn
+cat ~/log/$LOGNAME | mutt -s $MAILNAME wangbo@iscas.ac.cn jianmin@nfs.iscas.ac.cn xifei@nfs.iscas.ac.cn wangfeng@nfs.iscas.ac.cn wangyu@nfs.iscas.ac.cn yuanzhe@iscas.ac.cn

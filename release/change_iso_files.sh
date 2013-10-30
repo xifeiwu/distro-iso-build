6fa4f5efd69e57d8788177ae08615be408002c62
Jianmin <jianmin@nfs.iscas.ac.cn>
2013-10-30 16:24:30 +0800

    Fix bug 82.

--- release/change_iso_files.sh
+++ release/change_iso_files.sh
@@ -30,7 +30,7 @@ echo COS Desktop $COSVERSION "$COSVERSIONNAME" - Release i386 \(`date +%Y%m%d`\)
 echo COS Desktop $COSVERSION "$COSVERSIONNAME" - Release i386 \(`date +%Y%m%d`\)>$OUTPATH/mymint/.disk/mint4win
 echo www.iscas.ac.cn>$OUTPATH/mymint/.disk/release_notes_url
 sed -i 's/Linux Mint/COS Desktop/' $OUTPATH/mymint/boot/grub/loopback.cfg
-sed -i 's/Linux Mint 15 Cinnamon/COS Desktop "$COSVERSION"/' $OUTPATH/mymint/isolinux/isolinux.cfg
+sed -i "s/Linux Mint 15 Cinnamon/COS Desktop ${COSVERSION}/" $OUTPATH/mymint/isolinux/isolinux.cfg
 sed -i 's/Linux Mint/COS Desktop/' $OUTPATH/mymint/isolinux/isolinux.cfg
 echo Success generating info file.
 

bdae163ba1a7ad50b2ed55f40fc762eb55a85bb0
Jianmin <jianmin@nfs.iscas.ac.cn>
2013-12-06 11:27:35 +0800

    Change networking to networking-deprecated.

--- release/change_networking.sh
+++ release/change_networking.sh
@@ -7,6 +7,25 @@ if [ -z "$1" ] ; then
 fi
 
 OUTPATH=$1
-DISTURBPATH=$(cd "$(dirname $0)"; pwd)
 
-echo TODO: change networking to networking-delegate.
+echo Change networking to networking-deprecated.
+if [ -f $OUTPATH/etc/init/ufw.conf ] ; then
+    sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/etc/init/ufw.conf
+fi
+
+if [ -f $OUTPATH/etc/init/network-interface-security.conf ] ; then
+    sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/etc/init/network-interface-security.conf
+    sed -i "s/stopped networking /stopped networking-deprecated /" $OUTPATH/etc/init/network-interface-security.conf
+fi
+
+if [ -f $OUTPATH/etc/init/networking.conf ] ; then
+    cp $OUTPATH/etc/init/networking.conf $OUTPATH/etc/init/networking-deprecated.conf
+    sed -i "s/ networking - configure/ networking-deprecated configure/" $OUTPATH/etc/init/networking-deprecated.conf
+    rm $OUTPATH/etc/init/networking.conf
+fi
+
+if [ -f $OUTPATH/etc/init.d/networking ] ; then
+    mv $OUTPATH/etc/init.d/networking $OUTPATH/etc/init.d/networking-deprecated
+fi
+echo Finished changing networking to networking-deprecated
+echo Warning, you should reboot later.

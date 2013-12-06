a4fc419a7d7c2c01acba57e522eb6ba3f75c33f2
yunzhen <yunzhen_wx@nfshchina.com>
2013-12-06 17:11:04 +0800

    add sudo and path to this file

--- release/change_networking.sh
+++ release/change_networking.sh
@@ -9,23 +9,23 @@ fi
 OUTPATH=$1
 
 echo Change networking to networking-deprecated.
-if [ -f $OUTPATH/etc/init/ufw.conf ] ; then
-    sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/etc/init/ufw.conf
+if [ -f $OUTPATH/squashfs-root/etc/init/ufw.conf ] ; then
+    sudo sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/squashfs-root/etc/init/ufw.conf
 fi
 
-if [ -f $OUTPATH/etc/init/network-interface-security.conf ] ; then
-    sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/etc/init/network-interface-security.conf
-    sed -i "s/stopped networking /stopped networking-deprecated /" $OUTPATH/etc/init/network-interface-security.conf
+if [ -f $OUTPATH/squashfs-root/etc/init/network-interface-security.conf ] ; then
+    sudo sed -i "s/ networking)/ networking-deprecated)/" $OUTPATH/squashfs-root/etc/init/network-interface-security.conf
+    sudo sed -i "s/stopped networking /stopped networking-deprecated /" $OUTPATH/squashfs-root/etc/init/network-interface-security.conf
 fi
 
-if [ -f $OUTPATH/etc/init/networking.conf ] ; then
-    cp $OUTPATH/etc/init/networking.conf $OUTPATH/etc/init/networking-deprecated.conf
-    sed -i "s/ networking - configure/ networking-deprecated configure/" $OUTPATH/etc/init/networking-deprecated.conf
-    rm $OUTPATH/etc/init/networking.conf
+if [ -f $OUTPATH/squashfs-root/etc/init/networking.conf ] ; then
+    sudo cp $OUTPATH/squashfs-root/etc/init/networking.conf $OUTPATH/squashfs-root/etc/init/networking-deprecated.conf
+    sudo sed -i "s/ networking - configure/ networking-deprecated configure/" $OUTPATH/squashfs-root/etc/init/networking-deprecated.conf
+    sudo rm $OUTPATH/squashfs-root/etc/init/networking.conf
 fi
 
-if [ -f $OUTPATH/etc/init.d/networking ] ; then
-    mv $OUTPATH/etc/init.d/networking $OUTPATH/etc/init.d/networking-deprecated
+if [ -f $OUTPATH/squashfs-root/etc/init.d/networking ] ; then
+    sudo mv $OUTPATH/squashfs-root/etc/init.d/networking $OUTPATH/squashfs-root/etc/init.d/networking-deprecated
 fi
 echo Finished changing networking to networking-deprecated
 echo Warning, you should reboot later.

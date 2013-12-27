67b9e1162457eb38acb266aa0c181e0e64a5c96d
WangYu <wangyu@nfs.iscas.ac.cn>
2013-12-27 18:11:27 +0800

    change start menu applications

--- release/change_start_menu.sh
+++ release/change_start_menu.sh
@@ -29,10 +29,81 @@ if [ ! -x "$applicationPath" ] ; then
     echo "The applications directory does not exist!"
     exit -1
 fi
-cd $OUTPATH/squashfs-root/usr/share
-run_patch -p0 -i $DISTURBPATH/tmpfiles/applications.patch
-rm -r $OUTPATH/squashfs-root/usr/share/applications/*.rej
-rm -r $OUTPATH/squashfs-root/usr/share/applications/*.orig
+cd $OUTPATH/squashfs-root/usr/share/applications
+rm -f aptoncd.desktop
+cp $DISTURBPATH/tmpfiles/applications/baobab.desktop .
+cp $DISTURBPATH/tmpfiles/applications/bluetooth-sendto.desktop .
+cp $DISTURBPATH/tmpfiles/applications/bluetooth-wizard.desktop .
+cp $DISTURBPATH/tmpfiles/applications/caribou.desktop .
+cp $DISTURBPATH/tmpfiles/applications/cinnamon-control-center.desktop .
+cp $DISTURBPATH/tmpfiles/applications/cinnamon-network-panel.desktop .
+cp $DISTURBPATH/tmpfiles/applications/cinnamon-settings.desktop .
+cp $DISTURBPATH/tmpfiles/applications/cinnamon-sound-nua-panel.desktop .
+rm -f cinnamon-universal-access-panel.desktop
+cp $DISTURBPATH/tmpfiles/applications/cinnamon-user-accounts-panel.desktop .
+cp $DISTURBPATH/tmpfiles/applications/evince.desktop .
+rm -f fcitx-config-gtk3.desktop
+rm -f fcitx-configtool.desktop
+cp $DISTURBPATH/tmpfiles/applications/fcitx.desktop .
+rm -f file-roller.desktop
+cp $DISTURBPATH/tmpfiles/applications/firefox.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gcalctool.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gcr-prompter.desktop .
+rm -f gdebi.desktop
+cp $DISTURBPATH/tmpfiles/applications/gedit.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-disk-image-mounter.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-disks.desktop .
+rm -f gnome-font-viewer.desktop
+rm -f gnome-power-statistics.desktop
+cp $DISTURBPATH/tmpfiles/applications/gnome-screenshot.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-system-log.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-system-monitor.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-system-monitor-kde.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gnome-terminal.desktop .
+rm -f gnome-user-share-properties.desktop
+cp $DISTURBPATH/tmpfiles/applications/gparted.desktop .
+cp $DISTURBPATH/tmpfiles/applications/gthumb.desktop .
+rm -f gucharmap.desktop
+cp $DISTURBPATH/tmpfiles/applications/gufw.desktop .
+rm -f im-config.desktop
+rm -f itweb-settings.desktop
+cp $DISTURBPATH/tmpfiles/applications/mintBackup.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mintBackup_mime.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mintdrivers.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mintInstall.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mintInstall_kde.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mintInstall_mime.desktop .
+rm -f mintNanny.desktop
+cp $DISTURBPATH/tmpfiles/applications/mintsources.desktop .
+rm -f mintstick.desktop
+rm -f mintstick-kde.desktop
+cp $DISTURBPATH/tmpfiles/applications/mintUpdate.desktop .
+rm -f mintWelcome.desktop
+cp $DISTURBPATH/tmpfiles/applications/mono-runtime.desktop .
+cp $DISTURBPATH/tmpfiles/applications/mono-runtime-terminal.desktop .
+rm -f ndisgtk.desktop
+cp $DISTURBPATH/tmpfiles/applications/ndisgtk-kde.desktop .
+cp $DISTURBPATH/tmpfiles/applications/nemo-autorun-software.desktop .
+cp $DISTURBPATH/tmpfiles/applications/nemo.desktop .
+cp $DISTURBPATH/tmpfiles/applications/nm-applet.desktop .
+cp $DISTURBPATH/tmpfiles/applications/nm-connection-editor.desktop .
+rm -f openjdk-7-policytool.desktop
+rm -f seahorse.desktop
+cp $DISTURBPATH/tmpfiles/applications/session-properties.desktop .
+rm -f simple-scan.desktop
+cp $DISTURBPATH/tmpfiles/applications/synaptic.desktop .
+rm -f synaptic-kde.desktop
+cp $DISTURBPATH/tmpfiles/applications/system-config-printer.desktop .
+cp $DISTURBPATH/tmpfiles/applications/tomboy.desktop .
+cp $DISTURBPATH/tmpfiles/applications/totem.desktop .
+cp $DISTURBPATH/tmpfiles/applications/ubiquity-gtkui.desktop .
+rm -f upload-manager.desktop
+cp $DISTURBPATH/tmpfiles/applications/vino-preferences.desktop .
+rm -f xchat.desktop
+cp $DISTURBPATH/tmpfiles/applications/yelp.desktop .
+#run_patch -p0 -i $DISTURBPATH/tmpfiles/applications.patch
+#rm -r $OUTPATH/squashfs-root/usr/share/applications/*.rej
+#rm -r $OUTPATH/squashfs-root/usr/share/applications/*.orig
 echo "Patch applications directory successfully!"
 
 mdmApplicationsPath=$OUTPATH/squashfs-root/usr/share/mdm/applications
@@ -40,17 +111,19 @@ if [ ! -x "$mdmApplicationsPath" ] ; then
     echo "The mdm applications directory does not exist!"
     exit -1
 fi
-cd $OUTPATH/squashfs-root/usr/share
-run_patch -p0 -i $DISTURBPATH/tmpfiles/mdm_applications.patch
+cd $OUTPATH/squashfs-root/usr/share/mdm/applications
+rm -f mdmflexiserver.desktop
+cp  $DISTURBPATH/tmpfiles/mdm/applications/mdmsetup.desktop .
+#run_patch -p0 -i $DISTURBPATH/tmpfiles/mdm_applications.patch
 echo "Patch mdm applications directory successfully!"
 
 cp $DISTURBPATH/tmpfiles/yelp.desktop $OUTPATH/squashfs-root/usr/share/ubuntu-system-adjustments/yelp/
 echo "Reconfig help menu succefully!"
 
 #cp $DISTURBPATH/tmpfiles/org.cinnamon.gschema.xml $OUTPATH/squashfs-root/usr/share/glib-2.0/schemas/
-echo "Reconfig favorite menu succefully!"
-cp $DISTURBPATH/tmpfiles/icon.png $OUTPATH/squashfs-root/usr/lib/linuxmint/mintInstall/
-echo "Reconfig software center icon succefully!"
+#echo "Reconfig favorite menu succefully!"
+#cp $DISTURBPATH/tmpfiles/icon.png $OUTPATH/squashfs-root/usr/lib/linuxmint/mintInstall/
+#echo "Reconfig software center icon succefully!"
 
 #change start menu icon
 echo change squashfs-root/usr/share/cinnamon/theme/menu.png

1fea048fcd5e75ca16e777f05eeaf2b6d8e2e9df
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-01-07 15:32:57 +0800

    Fix wps fonts.

--- release/set_username_for_WPS.sh
+++ release/set_username_for_WPS.sh
@@ -6,8 +6,14 @@ if [ -z "$1" ] ; then
     exit -1
 fi
 
-OUTPATH=$1
+if [ -z "$2" ] ; then
+    echo error: No deb dir setting at second param.
+    exit -1
+fi
+
 DISTURBPATH=$(cd "$(dirname $0)"; pwd)
+OUTPATH=$1
+DEBDIR=$2
 
 echo set default username for WPS, add $OUTPATH/squashfs-root/etc/skel/.config/Software/Kingsoft.conf
 mypath=$OUTPATH/squashfs-root/etc/skel/.config/Software
@@ -15,3 +21,11 @@ if [ ! -x "$mypath" ] ; then
     mkdir $mypath
 fi
 cp $DISTURBPATH/tmpfiles/Kingsoft.conf $OUTPATH/squashfs-root/etc/skel/.config/Software/
+
+mkdir $OUTPATH/squashfs-root/app
+cp ${DEBDIR}/symbol-fonts_1.2_all.deb -a ${OUTPATH}/squashfs-root/app/
+#安装wps所需字体，该字体版权属于微软
+chroot $OUTPATH/squashfs-root /bin/bash -c "cd app && dpkg -i -E symbol-fonts_1.2_all.deb"
+chroot $OUTPATH/squashfs-root /bin/bash -c "rm -rf app"
+
+echo "Kingsoft.conf and wps-fonts installed successful!"

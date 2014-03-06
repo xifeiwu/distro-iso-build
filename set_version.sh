3fadf92af4c000ed0832eacce7761a27adc4a656
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-03-06 11:22:39 +0800

    Change the version name and version number.

--- set_version.sh
+++ set_version.sh
@@ -1,3 +1,10 @@
 #/bin/bash
-export COSVERSION=1.0
-export COSVERSIONNAME=IceBlue
+export OSNAME=cdos
+export OSFULLNAME=CDOS
+export OSISSUE=CDOS Team
+export OSNAMEZH=基础版
+export OSFULLNAMEZH=核高基桌面操作系统基础版
+export OSVERSION=1.0
+export OSVERSIONNAME=iceblue
+export OSVERSIONFullNAME=Iceblue
+

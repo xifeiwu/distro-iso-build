a89055ab85cea44f8b2eb7a4a411a1a99d317364
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-03-06 16:04:02 +0800

    Change the version name in set_version.sh

--- set_version.sh
+++ set_version.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 export OSNAME=cdos
 export OSFULLNAME=CDOS
-export OSISSUE=CDOS Team
+export OSISSUE=01Project
 export OSNAMEZH=基础版
 export OSFULLNAMEZH=核高基桌面操作系统基础版
 export OSVERSION=1.0
-export OSVERSIONNAME=iceblue
-export OSVERSIONFullNAME=Iceblue
-
+export OSVERSIONNAME=rat
+export OSVERSIONFULLNAME=Rat
+export OSRELEASEURL=release_URL

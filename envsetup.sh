00e923d21ca88f86d20bc66ffd09d860a58bfaac
WangYu <wangyu@nfs.iscas.ac.cn>
2014-04-18 15:35:20 +0800

    change some strings

--- envsetup.sh
+++ envsetup.sh
@@ -478,8 +478,7 @@ function mall()
             fi
             echo Finish checking building deb packages
         fi
-        echo ===
-        echo ===================Building all deb packages=======================
+        echo "===================Building all deb packages======================="
         echo
         for maindir in $BUILDOSDIRS
         do
